1ba2dc37d5c9779eac7683d0605a6975bd761138
rb <reutbechor@gmail.com>
2025-04-07 22:35:55 +0300

    Delete CPP_EX1_25 directory

--- CPP_EX1_25/Node/Node.cpp
+++
@@ -1,9 +0,0 @@
-#include "Node.h"
-
-namespace graph{
-    Node::Node(int vartexDest, int weight, Node* neighbor){
-        this-> vartexDest = vartexDest;
-        this-> weight = weight;
-        this-> neighbor = neighbor;
-    }
-}
